147bc0e136c522ddb6fcd3c725a425241da83050
chanfan1999 <1078324570@qq.com>
2020-08-31 13:54:52 +0800

    用同步的方式请求网页资源

--- java/com/chanfan/getyourclassschedule/NetModeFragment.kt
+++ java/com/chanfan/getyourclassschedule/NetModeFragment.kt
@@ -44,71 +44,30 @@ class NetModeFragment : Fragment() {
                 })
 
             login.setOnClickListener {
-                val ac = account.text.toString()
-                val pw = password.text.toString()
-                val rc = verifyCode.text.toString()
-                val loginForm = mapOf(
-                    "account" to ac, "password" to pw, "rancode" to rc,
-                    "client_id" to "", "response_type" to "", "redirect_url" to "", "jump" to ""
-                )
-                loginService.post(
-                    loginForm,
-                    "https://sso.scnu.edu.cn/AccountService/user/login.html"
-                ).enqueue(object : Callback<ResponseBody> {
-                    override fun onResponse(
-                        call: Call<ResponseBody>,
-                        response: Response<ResponseBody>
-                    ) {
-                        Toast.makeText(context, "正在处理", Toast.LENGTH_SHORT).show()
-                    }
 
-                    override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                        Toast.makeText(context, "网络好像出了问题呢~", Toast.LENGTH_SHORT).show()
-                    }
-                })
+                Thread {
+                    val ac = account.text.toString()
+                    val pw = password.text.toString()
+                    val rc = verifyCode.text.toString()
+                    val loginForm = mapOf(
+                        "account" to ac, "password" to pw, "rancode" to rc,
+                        "client_id" to "", "response_type" to "", "redirect_url" to "", "jump" to ""
+                    )
+                    loginService.post(
+                        loginForm,
+                        "https://sso.scnu.edu.cn/AccountService/user/login.html"
+                    ).execute()
 
-                loginService.get("https://sso.scnu.edu.cn/AccountService/openapi/onekeyapp.html?id=96")
-                    .enqueue(object : Callback<ResponseBody> {
-                        override fun onResponse(
-                            call: Call<ResponseBody>,
-                            response: Response<ResponseBody>
-                        ) {
-                            Toast.makeText(context, "正在获取", Toast.LENGTH_SHORT).show()
-                        }
-
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            Toast.makeText(context, "网络好像出了问题呢~", Toast.LENGTH_SHORT).show()
-                        }
-                    })
-                val formData = mapOf("xnm" to "2020", "xqm" to "3")
-                loginService.post(
-                    formData,
-                    "https://jwxt.scnu.edu.cn/kbcx/xskbcx_cxXsKb.html?gnmkdm=N253508"
-                )
-                    .enqueue(object : Callback<ResponseBody> {
-                        override fun onResponse(
-                            call: Call<ResponseBody>,
-                            response: Response<ResponseBody>
-                        ) {
-                            val data = response.body()?.string()
-                            if (data != null) {
-                                if (SHIPAI.isChecked)
-                                    ClassTableICAL.handleTextData(data, ClassTableICAL.SHIPAI)
-                                else
-                                    ClassTableICAL.handleTextData(data, ClassTableICAL.NANHAI)
-                            }
-
-                        }
-
-                        override fun onFailure(call: Call<ResponseBody>, t: Throwable) {
-                            Toast.makeText(context, "网络好像出了问题呢~", Toast.LENGTH_SHORT).show()
-                        }
-                    })
-
+                    loginService.get("https://sso.scnu.edu.cn/AccountService/openapi/onekeyapp.html?id=96")
+                        .execute()
+                    val formData = mapOf("xnm" to "2020", "xqm" to "3")
+                    val classData = loginService.post(
+                        formData,
+                        "https://jwxt.scnu.edu.cn/kbcx/xskbcx_cxXsKb.html?gnmkdm=N253508"
+                    ).execute().body()?.string()
+                }.start()
             }
 
-            //TODO 使用协程完善流程
-            //TODO 子线程中创建任务，主线程UI显示等待
 
         }
     }
