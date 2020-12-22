d83938e8dacb2ba278cdca03ad7155062534dbe9
Omnipresent <1999chanfan@gmail.com>
2020-12-22 17:06:35 +0800

    fix net mode links
    Signed-off-by: chanfan1999 <1999chanfan@gmail.com>

--- app/src/main/java/com/chanfan/getyourclassschedule/Logger.kt
+++
@@ -1,2 +0,0 @@
-package com.chanfan.getyourclassschedule
-
--- app/src/main/java/com/chanfan/getyourclassschedule/NetModeFragment.kt
+++ app/src/main/java/com/chanfan/getyourclassschedule/NetModeFragment.kt
@@ -1,7 +1,6 @@
 package com.chanfan.getyourclassschedule
 
 import android.Manifest
-import android.app.Activity
 import android.content.Context
 import android.content.pm.PackageManager
 import android.graphics.BitmapFactory
@@ -182,7 +181,6 @@ class NetModeFragment : Fragment() {
                 what = EXISTED
             })
         }
-
     }
 
     private fun hasPermissions(context: Context, vararg permissions: String): Boolean =
