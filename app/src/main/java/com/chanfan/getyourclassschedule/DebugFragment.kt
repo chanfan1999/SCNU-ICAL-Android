cd1379309ac3fea1ea2db69b630b108ac8e301b2
chanfan1999 <1999chanfan@gmail.com>
2020-12-28 14:20:08 +0800

    添加Debug模块
    Signed-off-by: chanfan1999 <1999chanfan@gmail.com>

---
+++ app/src/main/java/com/chanfan/getyourclassschedule/DebugFragment.kt
@@ -0,0 +1,62 @@
+package com.chanfan.getyourclassschedule
+
+import android.net.Uri
+import android.os.Bundle
+import android.provider.CalendarContract
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import kotlinx.android.synthetic.main.debug_fragment.*
+import java.io.File
+
+class DebugFragment : Fragment() {
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.debug_fragment, container, false)
+    }
+
+    override fun onViewCreated(view: View, savedInstanceState: Bundle?) {
+        super.onViewCreated(view, savedInstanceState)
+        val idList = ArrayList<String>()
+        deleteAllEvent.setOnClickListener {
+            context?.contentResolver?.query(
+                Uri.parse("content://com.android.calendar/events"), null,
+                null, null, null
+            )?.run {
+                while (moveToNext()) {
+                    val id = getString(getColumnIndex(CalendarContract.Events._ID))
+                    val title = getString(getColumnIndex("title"))
+                    val sl = getString(getColumnIndex("dtstart"))
+                    val se = getString(getColumnIndex("dtend"))
+                    Log.i("日历", "事件名称：$title")
+                    Log.i("日历", "事件id：$id")
+                    Log.i("日历", "开始时间：$sl")
+                    Log.i("日历", "结束时间：$se")
+                    idList.add(id)
+                }
+                close()
+            }
+            for (i in idList) {
+                context?.contentResolver?.delete(
+                    Uri.parse("content://com.android.calendar/events"),
+                    CalendarContract.Events._ID + "=?",
+                    arrayOf(i)
+                )
+                Log.i("已删除", i)
+            }
+
+            val f = File(context?.filesDir, "new.ics")
+            if (f.exists()) {
+                f.delete()
+                Log.i("已删除", "清理成功")
+            }
+
+        }
+    }
+
+}
